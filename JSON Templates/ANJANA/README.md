b0a44d94bb9b97a89d4beaec8737b8cb29b13c1f
Konstantinos Chousos <K.chousos@protonmail.com>
2025-06-07 19:39:55 +0300

    k, l and t set in templates, script acts accordingly

--- JSON Templates/ANJANA/README.md
+++ JSON Templates/ANJANA/README.md
@@ -1,4 +1,4 @@
-# Development setup
+# Setup
 
 1. Create a Python3.10 virtual environment in `.venv` and activate it.
 
@@ -7,8 +7,38 @@ $ python3.10 -m venv .venv
 $ source .venv/bin/activate # for bash
 ```
 
-2. Install ANJANA.
+1. Install the dependencies.
 
 ``` shell
-pip install anjana
+pip install -r requirements.txt
 ```
+
+# JSON templates structure
+
+Each template has the necessary attributes for ANJANA to function. Namely, it *must* include:
+
+1. `data`: The path to the dataset (csv files only for now).
+2. `ident`: The identifier's column name.
+3. `quasi_ident`: A list of column names of the dataset's QAs.
+4. `supp_level`: The suppression level for ANJANA (float between 0 and 100).
+5. `hierarchies`: A dictionary with QAs as keys and the path to the corresponding hierarchy csv file as value.
+6. `sens_att`: The sensitive attribute column name for $l$-diversity and $t$-closeness (not needed for $k$-anonymity).
+7. `k`: The value of $k$ for $k$-anonymity.
+8. `l`: If not set to `False`, the value of $l$ for $l$-diversity.
+9. `t`: If not set to `False`, the value of $t$ for $t$-closeness.
+
+Set `l` and `t` to `False` if only $k$-anonymity is needed.
+
+# Example usage
+
+The provided templates use the `adult.csv` dataset [1] and use the values used in ANJANA's official examples [2]. To use a specific template, pass it as a CLI argument to the `wrapper.py` script, like so:
+
+``` shell
+$ python ./wrapper.py templates/t-closeness.json
+```
+
+# References
+
+[1] R. K. Barry Becker, “Adult.” UCI Machine Learning Repository, 1996. doi: 10.24432/C5XW20. Available: https://archive.ics.uci.edu/dataset/2
+
+[2] J. Sáinz-Pardo Díaz and Á. López García, “Anjana.” May 2024. Available: https://github.com/IFCA-Advanced-Computing/anjana
--- JSON Templates/ANJANA/wrapper.py
+++ JSON Templates/ANJANA/wrapper.py
@@ -1,13 +1,37 @@
 import pandas as pd
-from anjana.anonymity import k_anonymity
+from anjana.anonymity import k_anonymity, l_diversity, t_closeness
 import pycanon
 import time
 import json
+import argparse
 
-with open('templates/k_anonymity.json', 'r') as file:
+# Set up the argument parser
+parser = argparse.ArgumentParser(description='Load hierarchies from a JSON file.')
+parser.add_argument('json_file', type=str, help='Path to the JSON file')
+
+# Parse the command line arguments
+args = parser.parse_args()
+json_file_path = args.json_file
+
+# Template file reading
+with open(json_file_path, 'r') as file:
     values = json.load(file)
 
-data = pd.read_csv(values['data'])  # 32561 rows
+data = pd.read_csv(values['data'])
+quasi_ident = values['quasi_ident']
+ident = values['ident']
+k = values['k']
+l = values['l']
+t = values['t']
+supp_level = values['supp_level']
+sens_att = values.get('sens_att')
+
+hierarchies = {
+    key: dict(pd.read_csv(value, header=None))
+    for key, value in values['hierarchies'].items()
+}
+
+# TODO: remove this somehow
 data.columns = data.columns.str.strip()
 cols = [
     'workclass',
@@ -19,30 +43,34 @@ cols = [
 ]
 for col in cols:
     data[col] = data[col].str.strip()
-print(data)  # 32561 rows
-quasi_ident = values['quasi_ident']
-ident = values['ident']
-k = 10
-supp_level = 50
-
-hierarchies = {
-    key: dict(pd.read_csv(value, header=None))
-    for key, value in values['hierarchies'].items()
-}
 
 start = time.time()
+
 data_anon = k_anonymity(data, ident, quasi_ident, k, supp_level, hierarchies)
+if not l == False:
+    data_anon = l_diversity(
+        data_anon, ident, quasi_ident, sens_att, k, l, supp_level, hierarchies
+    )
+    if not t == False:
+        data_anon = t_closeness(
+            data_anon, ident, quasi_ident, sens_att, k, t, supp_level, hierarchies
+        )
+
 end = time.time()
-print(f'Elapsed time: {end-start}')
-print(f'Value of k calculated: {pycanon.anonymity.k_anonymity(data_anon, quasi_ident)}')
 
-# Elapsed time: 0.9592475891113281
-# Value of k calculated: 10
-
-print(f'Number of records suppressed: {len(data) - len(data_anon)}')
+print(f"Elapsed time: {end-start}")
 print(
-    f'Percentage of records suppressed: {100 * (len(data) - len(data_anon)) / len(data)} %'
+    f"Value of k calculated: "
+    f"\t{pycanon.anonymity.k_anonymity(data_anon, quasi_ident)}"
 )
-
-# Number of records suppressed: 14234
-# Percentage of records suppressed: 43.71487362181751 %
+if not l == False:
+    print(
+        f"Value of l-diversity: "
+        f"\t{pycanon.anonymity.l_diversity(data_anon, quasi_ident, [sens_att])}"
+    )
+    if not t == False:
+        print(
+            f"Value of t-closeness: "
+            f"\t{pycanon.anonymity.t_closeness(data_anon, quasi_ident, [sens_att])}"
+        )
+        
