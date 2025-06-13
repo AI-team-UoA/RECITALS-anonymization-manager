12cd58b35d0f747fb22ce8b309f8ba8a3281feef
Konstantinos Chousos <K.chousos@protonmail.com>
2025-06-14 00:11:05 +0300

    initial support for DP

---
+++ ANJANA/wrapper.py
@@ -0,0 +1,89 @@
+import os
+import pandas as pd
+from anjana.anonymity import k_anonymity, l_diversity, t_closeness
+import pycanon
+import time
+import json
+import argparse
+
+# Set up the argument parser
+parser = argparse.ArgumentParser(description="Load hierarchies from a JSON file.")
+parser.add_argument("json_file", type=str, help="Path to the JSON file")
+
+# Parse the command line arguments
+args = parser.parse_args()
+json_file_path = args.json_file
+
+# Template file reading
+with open(json_file_path, "r") as file:
+    values = json.load(file)
+
+data = pd.read_csv(values["data"])
+quasi_ident = values["quasi_ident"]
+ident = values["ident"]
+k = values["k"]
+l = values.get("l")
+t = values.get("t")
+supp_level = values["supp_level"]
+sens_att = values.get("sens_att")
+
+hierarchies = {
+    key: dict(pd.read_csv(value, header=None))
+    for key, value in values["hierarchies"].items()
+}
+
+# Strip whitespace from column names
+data.columns = data.columns.str.strip()
+
+# Strip whitespace from all string (object) columns
+str_cols = data.select_dtypes(include=["object", "string"]).columns
+data[str_cols] = data[str_cols].apply(lambda col: col.str.strip())
+
+start = time.time()
+
+data_anon = k_anonymity(data, ident, quasi_ident, k, supp_level, hierarchies)
+if not l == None:
+    data_anon = l_diversity(
+        data_anon, ident, quasi_ident, sens_att, k, l, supp_level, hierarchies
+    )
+    if not t == None:
+        data_anon = t_closeness(
+            data_anon, ident, quasi_ident, sens_att, k, t, supp_level, hierarchies
+        )
+
+end = time.time()
+
+print(f"Elapsed time: {end - start}")
+print(
+    f"Value of k calculated: \t{pycanon.anonymity.k_anonymity(data_anon, quasi_ident)}"
+)
+if not l == None:
+    print(
+        f"Value of l-diversity: "
+        f"\t{pycanon.anonymity.l_diversity(data_anon, quasi_ident, [sens_att])}"
+    )
+    if not t == None:
+        print(
+            f"Value of t-closeness: "
+            f"\t{pycanon.anonymity.t_closeness(data_anon, quasi_ident, [sens_att])}"
+        )
+
+# Create the output directory if it doesn't exist
+output_dir = "anonymized_data"
+os.makedirs(output_dir, exist_ok=True)
+
+# Create the output file path
+base_name = os.path.splitext(os.path.basename(values["data"]))[0]
+filename = f"{base_name}_k{k}"
+
+if l is not None:
+    filename += f"_l{l}"
+
+if t is not None:
+    filename += f"_t{t}"
+
+filename += ".csv"
+output_file_name = os.path.join(output_dir, filename)
+data_anon.to_csv(output_file_name, index=False)
+
+print(f"Anonymized data saved to: {output_file_name}")
