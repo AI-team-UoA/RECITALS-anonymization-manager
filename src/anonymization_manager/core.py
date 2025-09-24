cfc38a7a90a89ab5605af77dd32e219b56b4cf24
Konstantinos Chousos <K.chousos@protonmail.com>
2025-09-24 13:21:30 +0300

    fixed AnonymizationConfig docstring

--- src/anonymization_manager/core.py
+++ src/anonymization_manager/core.py
@@ -11,24 +11,25 @@ import pandas as pd
 @dataclass
 class AnonymizationConfig:
     """
-    Configuration for an anonymization workflow. Contains:
+    Configuration for an anonymization workflow.
 
-    data (str): The path to the initial dataset. CSV, Excel, JSON and
-        SQLite3 formats are supported.
-    identifiers (dict[list[str]]): A dictionary containing lists with
-        the IDs (ids), the quasi-IDs (qids), the sensitive attributes
-        (satts) and insensitive attributes (iatts).
-    hierarchies (dict[str]): A dictionary with the attributes as keys
-        and the path to the CSV hierarchies as data.
-    Parameters (dict[float]): A dictionary with all necessary parameters
-        for the desired anonymity models (k for k-anonymity, l for
-        l-diversity, t for t-closeness).
-    suppression (int, optional): The percentage of suppression to be
-        applied. Defaults to TBD.
-    anonymized_data (str, optional): The path for the resulting dataset.
-        Defaults to "./results/<dataset>_k-<k>_l-<l>_t-<t>.<extension>".
-    backend (str, optional): The backend library to be used, between
-        ARX and ANJANA. Defaults to TBD.
+    Args:
+        data (str): The path to the initial dataset. CSV, Excel, JSON and
+            SQLite3 formats are supported.
+        identifiers (dict[list[str]]): A dictionary containing lists with
+            the IDs (ids), the quasi-IDs (qids), the sensitive attributes
+            (satts) and insensitive attributes (iatts).
+        hierarchies (dict[str]): A dictionary with the attributes as keys
+            and the path to the CSV hierarchies as data.
+        Parameters (dict[float]): A dictionary with all necessary parameters
+            for the desired anonymity models (k for k-anonymity, l for
+            l-diversity, t for t-closeness).
+        suppression (int, optional): The percentage of suppression to be
+            applied. Defaults to TBD.
+        anonymized_data (str, optional): The path for the resulting dataset.
+            Defaults to `./results/<dataset>_k-<k>_l-<l>_t-<t>.<extension>`.
+        backend (str, optional): The backend library to be used, between
+            ARX and ANJANA. Defaults to TBD.
     """
 
     data: str
@@ -55,6 +56,7 @@ class AnonymizationManager:
                 identifiers, hierarchies, parameters, suppression, output path,
                 and backend.
         """
+
         self.config = config
         ...
 
