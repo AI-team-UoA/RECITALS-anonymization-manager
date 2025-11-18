e20eaf09ba36edd53447da61d37a4959192d6bde
Dimitris Pavlou <sdi2100151@di.uoa.gr>
2025-11-19 01:20:06 +0200

    Added/Cleaned code for the validation of the configuration object.

--- src/anonymization_manager/config.py
+++ src/anonymization_manager/config.py
@@ -2,7 +2,7 @@ import json
 from dataclasses import dataclass
 
 import pandas as pd
-
+import os
 
 @dataclass
 class AnonymizationConfig:
@@ -45,46 +45,20 @@ class AnonymizationConfig:
         """
         Default initializer.
         """
-        # --- Checks if k is correct ---
-        if k is not None:
-            if not isinstance(k, int) or  k <= 0:
-                raise ValueError(f"The parameter for k-anonymity must be a positive integer, but got {k} instead!")
-        
-        # --- Checks if l is correct ---
-        if l is not None:
-            if not isinstance(l, int) or  l <= 0:
-                raise ValueError(f"The parameter for l-diversity must be a positive integer, but got {l} instead!")
-        
-        # --- Checks if t is correct ---
-        if t is not None:
-            if not isinstance(t, float) or not (0 <= t <= 1):
-                raise ValueError(f"The parameter for t-closeness must be a float in range [0,1], but got {t} instead!")
-        
-        # --- Checks if the suppresion limit is correct ---
-        if suppression_limit is not None:
-            if not isinstance(suppression_limit, int) or not (0 <= suppression_limit <= 100):
-                raise ValueError(f"The parameter for suppresion limit must be an integer in range [0,100], but got {suppression_limit} instead!")
-
-        # --- Checks if the backend is correct ---    
-        if backend is not None:
-            if backend != "arx" and backend != "anjana":
-                raise ValueError(f"The parameter for the backend must be either the string arx or anjana, but got {backend} instead!")
-            
         self.data = data
         self.identifiers = identifiers
         self.quasi_identifiers = quasi_identifiers
         self.sensitive_attributes = sensitive_attributes
         self.insensitive_attributes = insensitive_attributes
-        hierarchies = hierarchies
-        self.hierarchies = {
-            key: dict(pd.read_csv(path, header=None))
-            for key, path in hierarchies.items()
-        }
+        self.hierarchies = hierarchies
         self.k = k
         self.l = l
         self.t = t
         self.suppression_limit = suppression_limit
         self.backend = backend
+
+        # Validates the config file.
+        self._validate()
 
     @classmethod
     def from_json(cls, json_path: str):
@@ -107,3 +81,129 @@ class AnonymizationConfig:
         )
 
         return config
+
+    def _validate(self) -> None:
+        """A helper method used for validating the configuration."""
+        self._validate_parameters()
+        self._validate_attributes()
+        self._validate_dataset()
+        self._validate_hierarchies()
+        
+    def _validate_parameters(self) -> None:
+        """Validates k, l, t, suppression and backend parameters."""
+
+        # --- Checks if k is correct ---
+        if self.k is not None:
+            if not isinstance(self.k, int) or  self.k <= 0:
+                raise ValueError(
+                    f"The parameter for k-anonymity must be a positive integer, but got {self.k!r} instead!"
+                )
+        
+        # --- Checks if l is correct ---
+        if self.l is not None:
+            if not isinstance(self.l, int) or  self.l <= 0:
+                raise ValueError(
+                    f"The parameter for l-diversity must be a positive integer, but got {self.l!r} instead!"
+                )
+        
+        # --- Checks if t is correct ---
+        if self.t is not None:
+            if not isinstance(self.t, float) or not (0 <= self.t <= 1):
+                raise ValueError(
+                    f"The parameter for t-closeness must be a float in range [0,1], but got {self.t!r} instead!"
+                )
+        
+        # --- Checks if the suppression limit is correct ---
+        if self.suppression_limit is not None:
+            if not isinstance(self.suppression_limit, int) or not (0 <= self.suppression_limit <= 100):
+                raise ValueError(
+                    f"The parameter for suppression limit must be an integer in range [0,100], but got {self.suppression_limit!r} instead!"
+                )
+
+        # --- Checks if the backend is correct ---    
+        if self.backend not in [None, "arx", "anjana"]:
+            raise ValueError(
+                f"The backend must be either 'arx' or 'anjana', but got {self.backend!r} instead!"
+            )
+    
+    def _validate_attributes(self) -> None:
+        """Validates the identifiers, quasi-identifiers, sensitive attributes and insensitive attributes."""
+
+        # Checks that the attributes are provided using lists.
+        for attr_list in [self.identifiers, self.quasi_identifiers, self.sensitive_attributes, self.insensitive_attributes]:
+            if not isinstance(attr_list, list):
+                raise ValueError(
+                    f"Attributes must be provided using lists, but got {attr_list!r} instead!"
+                )
+            
+        # --- Collects the attributes ---
+        attrs = (
+            self.identifiers+
+            self.quasi_identifiers+
+            self.sensitive_attributes+
+            self.insensitive_attributes
+        )
+
+        # --- Checks that each attribute is a string ---
+        for attr in attrs:
+            if not isinstance(attr, str):
+                raise ValueError(
+                    f"Attribute names must be strings, but got {attr!r} instead!"
+                )
+        
+        # --- Checks that the attribute names do not overlap.
+        if len(attrs) != len(set(attrs)):
+            raise ValueError(
+                f"Attribute names must be unique across all types!"
+            )
+
+    def _validate_dataset(self) -> None:
+        """Validates the dataset."""
+
+        # --- Checks that the dataset path is a string ---
+        if not isinstance(self.data, str):
+            raise ValueError(
+                f"The dataset path must be provided as a string, but got {self.data!r} instead!"
+            )
+        
+        # --- Checks that the dataset file exists.
+        if not os.path.exists(self.data):
+            raise ValueError(
+                f"The dataset could not be located at {self.data!r}!"
+            )
+    
+    def _validate_hierarchies(self) -> None:
+        """Validates the hierarchies."""
+
+        # --- Checks that the hierarchy format is valid ---
+        if not isinstance(self.hierarchies, dict):
+            raise ValueError(
+                f"The hierarchies must be provided in a dictionary format mapping quasi-identifiers to hierarchy file paths!"
+            )
+
+        # --- Checks if the hierarchies are valid ---
+        for qid, hierarchy_path in self.hierarchies.items():
+            # --- Checks that the quasi-identifier is a string ---
+            if not isinstance(qid, str):
+                raise ValueError(
+                    f"Hierarchy quasi-identifier keys must be strings, but got {qid!r} instead!"
+                )
+            
+            # --- Checks that the quasi-identifier exists ---
+            if qid not in self.quasi_identifiers:
+                raise ValueError(
+                    f"Cannot create hierarchy for {qid!r}, since it is not a quasi-identifier!"
+                )
+            
+
+            # --- Checks that the hierarchy path is a string ---
+            if not isinstance(hierarchy_path, str):
+                raise ValueError(
+                    f"The hierarchy path for {qid!r} must be a string, but got {hierarchy_path!r} instead!"
+                )
+            
+            # --- Checks that the hierarchy path exists.
+            if not os.path.exists(hierarchy_path):
+                raise ValueError(
+                    f"Cannot create hierarchy for {qid!r}, the path {hierarchy_path!r} could not be located!"
+                )
